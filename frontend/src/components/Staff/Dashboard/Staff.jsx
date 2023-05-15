772fdce3c4355a858b1a270c8cf520f66b71596d
Yazak <yoosufyazak@gmail.com>
2023-05-15 10:55:32 +0500

    init base dir

--- frontend/src/components/Staff/Dashboard/Staff.jsx
+++ frontend/src/components/Staff/Dashboard/Staff.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from "react";
+
+//my components
+import Dashboard from "./Dashboard";
+import Login from "../Login/Login";
 
 const Staff = () => {
-  return (
-    <div>Staff</div>
-  )
-}
+    const [auth, setAuth] = useState(false); //check if user auth state
+    return (
+        <>
+            <div>{auth ? <Dashboard /> : <Login />}</div>
+        </>
+    );
+};
 
-export default Staff
+export default Staff;
